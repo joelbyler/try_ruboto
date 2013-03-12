4d6e5d0afdd25fd9a1fc7fa038fe32eaf155f7d4
Joel Byler <joelbyler@gmail.com>
2013-03-12 13:06:49 -0400

    initial load

---
+++ src/com/test/me/TestActivity.java
@@ -0,0 +1,10 @@
+package com.test.me;
+
+import android.os.Bundle;
+
+public class TestActivity extends org.ruboto.EntryPointActivity {
+	public void onCreate(Bundle bundle) {
+		getScriptInfo().setRubyClassName(getClass().getSimpleName());
+	    super.onCreate(bundle);
+	}
+}
